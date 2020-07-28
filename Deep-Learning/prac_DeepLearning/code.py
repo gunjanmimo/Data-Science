74d2924ace34a26f136e633e1b545de26c886e06
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-07-28 14:09:23 +0530

    mew

--- Deep-Learning/prac_DeepLearning/code.py
+++ Deep-Learning/prac_DeepLearning/code.py
@@ -1,40 +1,41 @@
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Sequential
-import keras
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 import pandas as pd
 
 data = pd.read_csv("nba_logreg.csv")
 
+
+
 # missing value checking
-data.drop('3P%', axis=1, inplace=True)
+data.drop('3P%',axis=1,inplace=True)
 
 
 # we do not need the name column
-X = data.iloc[:, 1:19].values
-y = data.iloc[:, 19].values
+X=data.iloc[:,1:19].values
+y=data.iloc[:,19].values
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
+
+from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+X_test= sc.transform(X_test)
 
 
-# ANN
+#ANN
+import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
-bclfr = Sequential()
+bclfr =Sequential()
 
-bclfr.add(Dense(units=10, kernel_initializer='uniform',
-                activation='relu', input_dim=18))
-bclfr.add(Dense(units=10, kernel_initializer='uniform', activation='relu'))
-bclfr.add(Dense(units=10, kernel_initializer='uniform', activation='sigmoid'))
-bclfr.compile(optimizer='adam', loss='binary_crossentropy',
-              metrics=['accuracy'])
-bclfr.fit(X_train, y_train, batch_size=10, epochs=100)
+bclfr.add(Dense(units= 10,kernel_initializer='uniform',activation='relu',input_dim=18))
+bclfr.add(Dense(units=10,kernel_initializer='uniform',activation='relu'))
+bclfr.add(Dense(units=10,kernel_initializer='uniform',activation='sigmoid'))
+bclfr.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+bclfr.fit(X_train,y_train,batch_size=10,epochs=100)
 
 
-y_pred = bclfr.predict(X_test)
+
+y_pred= bclfr.predict(X_test)
---
+++ Deep-Learning/prac_DeepLearning/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+sc = StandardScaler()
+# X_train = sc.fit_transform(X_train)
+# X_test = sc.transform(X_test)
